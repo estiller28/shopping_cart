704b4995bfdfe50cfa367251b8edb67ac3fb7f3d
Daniel <estillerdaniel@gmail.com>
2021-04-22 09:15:27 -0700

    add to cart function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //variables
 
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+    shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 
@@ -33,5 +34,34 @@ function buyCourse(e){
 //reading the couses in the markup
 
 function getCourseInfo(course){
-    console.log(course);
+    const courseInfo = {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')
+
+    }
+    
+    addIntoCart(courseInfo);
+}
+
+//add to cart
+
+function addIntoCart(course){
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+       <tr>
+            <td>
+                <img src="${course.image}" width = 100px>
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+       </tr> 
+    `;
+    
+    shoppingCartContent.appendChild(row);
 }
